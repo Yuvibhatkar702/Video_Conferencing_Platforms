036d851e0830d2ac2a1a42d2459e39512bf691f2
Yuvraj Bhatkar <yuvibhatkar702@gmail.com>
2025-02-13 13:26:56 +0530

    first commit

--- Frontend/Zoom/src/pages/authentication.jsx
+++ Frontend/Zoom/src/pages/authentication.jsx
@@ -3,18 +3,28 @@ import React from 'react'
 export default function Authentication() {
 
 
-  const [username,setUsetname] = React.useState();
-  const [password,setPassword] = React.useState();
-  const [name,setName] = Reaact.useState();
-  const [error,setError] = React.useState();
+  // const [username,setUsetname] = React.useState();
+  // const [password,setPassword] = React.useState();
+  // const [name,setName] = Reaact.useState();
+  // const [error,setError] = React.useState();
+  // const [massage,setMassage] = React.useState();
 
-  const [formState, setFormState] = React.useState(0);
+  // const [formState, setFormState] = React.useState(0);
 
+  // const [open, setOpen] = React.useState(false);
 
   return (
     <div className="login">
         <div className="right">
+            {/* <div>
+              <Button id='btn' variant={formState === 0 ? "contained" : ""} onClick={() => {setFormState(0)}}>Sign In</Button>
+              <Button id='btn' variant={formState === 1 ? "contained" : ""} onClick={() => {setFormState(1)}}>Sign Up</Button>
+            </div> */}
+
             <form method="post" action="/login/check">
+              {/* { formState === 1 ? 
+                <input type='text' name='fullName' placeholder='Full Name' id='text' />
+               : null   } */}
                 <input type="text" name="email" placeholder="Email address or phone number" id="text" size="50" />
                 <input type="password" name="password" placeholder="password" id="text" />
                 <button>Log in</button>
